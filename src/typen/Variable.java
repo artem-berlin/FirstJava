b36c5d8d2cb609547442f06cbbdfd1da0017358e
artem-berlin <art.tester@gmail.com>
2022-02-01 16:20:06 +0100

    + Package

---
+++ src/typen/Variable.java
@@ -0,0 +1,13 @@
+package typen;
+
+public class Variable {
+    public static void main(String[] args)
+    {
+        int a= 2;
+        int b = 3;
+        int summe = a + b;
+
+
+        System.out.println("Hallo" + " " + summe);
+    }
+}
